e7014b417b803295ae66327add025f4f9e01e56b
Antoine Leguy <aleguy.pro@gmail.com>
2018-07-11 11:27:05 +0200

    adding GET of one product and test

---
+++ tests/test_views.py
@@ -0,0 +1,23 @@
+from flask_testing import TestCase
+from wsgi import app
+
+class TestViews(TestCase):
+    def create_app(self):
+        app.config['TESTING'] = True
+        return app
+
+    def test_products_json(self):
+        response = self.client.get("/api/v1/products")
+        products = response.json
+        self.assertIsInstance(products, list)
+        self.assertGreater(len(products), 3) # 3 is not a mistake here.
+
+
+    def test_get_404_when_product_not_exist(self):
+        response = self.client.get("/api/v1/products/456")
+        self.assertEquals(response.status_code,404)
+
+
+    def test_get_200_when_product_exists(self):
+        response = self.client.get("/api/v1/products/1")
+        self.assertEquals(response.status_code,200)
