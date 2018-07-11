87d387efebcbecf57144eb1dc3ae39af3b25ec95
Antoine Leguy <aleguy.pro@gmail.com>
2018-07-11 10:39:33 +0200

    add product route

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,18 @@
 from flask import Flask
+import json
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return "Hello World !"
+
+@app.route('/api/v1/products')
+def return_product:
+    the_products = [
+        { 'id': 1, 'name': 'Skello' },
+        { 'id': 2, 'name': 'Socialive.tv' }
+    ]
+    return the_products.dumps(the_products)
+
+
