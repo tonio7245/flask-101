e3aa8768033803f66218dcfae3b88e24f173656f
Antoine Leguy <aleguy.pro@gmail.com>
2018-07-11 14:50:41 +0200

    update route and tests

--- tests/test_views.py
+++ tests/test_views.py
@@ -36,3 +36,20 @@ class TestViews(TestCase):
         response = self.client.post('/api/v1/products/', data=dict(id=6,name='chose'))
         self.assertEquals(response.status_code,201)
         self.assertEquals(response.json,{ 'id': 6, 'name': 'chose' })
+
+    def test_get_422_if_empty(self):
+        response = self.client.patch('/api/v1/products/1', data=dict(name=''))
+        self.assertEquals(response.status_code,422)
+        response = self.client.patch('/api/v1/products/1')
+        self.assertEquals(response.status_code,422)
+
+
+    def test_get_204_update(self):
+        response = self.client.get('/api/v1/products/1')
+        self.assertEquals(response.json,{ 'id': 1, 'name': 'Skello' })
+        response = self.client.patch('/api/v1/products/1', data=dict(name='Skella'))
+        self.assertEquals(response.status_code,204)
+        response = self.client.get('/api/v1/products/1')
+        self.assertEquals(response.json,{ 'id': 1, 'name': 'Skella' })
+
+
